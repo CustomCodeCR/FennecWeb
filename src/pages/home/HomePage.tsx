3363fc65ced8671b44611bb999b5609f2d7734f8
Mati Solano <matias.solano1408@gmail.com>
2026-08-11 13:32:20 -0600

    refactor: migrate home hero to TypeScript

---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,15 @@
+// Importa la sección principal
+import HeroSection from '../../components/hero/HeroSection'
+
+// Página principal
+function HomePage() {
+  return (
+    <main>
+      {/* Sección principal con video */}
+      <HeroSection />
+    </main>
+  )
+}
+
+// Exporta la página
+export default HomePage
