529dd838fbb30ee6aac394130811fa4e6ab2aac1
MauLang18 <maulangbonilla.18@gmail.com>
2026-08-14 11:14:15 -0600

    fix dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import Footer from './complement/footer/Footer'
-import HomePage from './pages/home/HomePage'
-import Apps from './Navbar'
+import Footer from "./complement/footer/Footer";
+import HomePage from "./pages/home/HomePage";
+import Apps from "./Navbar";
+import ContactForm from "./Complements/Contact/ContactForm";
 
 function App() {
   return (
     <>
       <Apps />
       <HomePage />
+      <ContactForm />
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
